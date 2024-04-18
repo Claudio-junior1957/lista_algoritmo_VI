fcaf2f7efc7a9f234a02bb0e76dfcbc30f03e29a
Claudio Junior <134631237+Claudio-junior1957@users.noreply.github.com>
2024-04-17 21:41:59 -0300

    Update 2_divisao_exata.c
    Adicionei a biblioteca #include<locale.h> e setlocale, permitindo assim o uso de acenttos da língua portuguesa.

--- 2_divisao_exata.c
+++ 2_divisao_exata.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-
+#include<locale.h>
 int main(){
-
+    setlocale (LC_ALL, "Portuguese");
     int num,de,result;
 
-   
     printf("Insira o denominador: ");
     scanf("%i",&num);
 
     printf("Insira o numerador: ");
     scanf("%i",&de);
-
-    
 
     if(num % de == 0){
         printf("e exato");
@@ -21,12 +18,5 @@ int main(){
         }else{
             printf("nao e possivel dividir por 0");
         }
-    }
-
-    
-
-
-
-
-
+    }    
 }
